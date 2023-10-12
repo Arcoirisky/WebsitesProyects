3b2421210bd2d4a9f00511210a195b37c70b9a1a
Jessica Hormazabal <arcoirisky@MacBook-Pro-de-Jessica.local>
2023-10-12 12:03:28 -0300

    feature(apple-eCommerce): controls working

--- Apple-e commerce/Apple eCommerce/script.js
+++ Apple-e commerce/Apple eCommerce/script.js
@@ -25,3 +25,60 @@ const slideshow = () => {
 
 slideshow();
 // End of Slideshow
+
+// Cube
+let y = 20;
+let x = 0;
+let z = 0;
+let bool = true;
+let interval;
+
+const cube = document.querySelector(".cube");
+
+document.querySelector(".top-x-control").addEventListener("click", () => {
+  cube.style.transform = `rotateX(${(x += 20)}deg) rotateY(${y}deg) rotateZ(${z}deg)`;
+});
+
+document.querySelector(".bottom-x-control").addEventListener("click", () => {
+  cube.style.transform = `rotateX(${(x -= 20)}deg) rotateY(${y}deg) rotateZ(${z}deg)`;
+});
+
+document.querySelector(".left-y-control").addEventListener("click", () => {
+  cube.style.transform = `rotateY(${(y -= 20)}deg) rotateX(${x}deg) rotateZ(${z}deg)`;
+});
+
+document.querySelector(".right-y-control").addEventListener("click", () => {
+  cube.style.transform = `rotateY(${(y += 20)}deg) rotateX(${x}deg) rotateZ(${z}deg)`;
+});
+
+document.querySelector(".top-z-control").addEventListener("click", () => {
+  cube.style.transform = `rotateZ(${(z -= 20)}deg) rotateX(${x}deg) rotateY(${y}deg)`;
+});
+
+document.querySelector(".bottom-z-control").addEventListener("click", () => {
+  cube.style.transform = `rotateZ(${(z += 20)}deg) rotateX(${x}deg) rotateY(${y}deg)`;
+});
+
+const playPause = () => {
+  if (bool) {
+    interval = setInterval(() => {
+      cube.style.transform = `rotateY(${y++}deg) rotateX(${x}deg) rotateZ(${z}deg)`;
+    }, 100);
+  } else {
+    clearInterval(interval);
+  }
+};
+
+playPause();
+
+document.querySelector(".controls").addEventListener("mouseover", () => {
+  bool = false;
+  playPause();
+});
+
+document.querySelector(".controls").addEventListener("mouseout", () => {
+  bool = true;
+  playPause();
+});
+
+// End of Cube
